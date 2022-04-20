7d89d64c7a243bb12ba70f403dca940ebbdfba8a
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-04-20 14:58:19 +0200

    added bcrypt and cookie session
    Co-authored-by: Anna Özmehak <A-Ozmehak@users.noreply.github.com>
    Co-authored-by: Jannie Bäckman Kuurne <Jannie87@users.noreply.github.com>

---
+++ server/routes/posts.js
@@ -0,0 +1,93 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const routes = express.Router();
+const app = express();
+
+app.use(express.json());
+
+let posts = [
+  {
+    username: "Ella",
+    title: "Golden retriever for adoption",
+    description: "A golden retriver puppy that needs a new home.",
+    id: uuidv4(),
+    date: new Date(),
+  },
+];
+
+routes.get("/posts", (req, res) => {
+  console.log(posts);
+  res.status(200);
+  res.json(posts);
+});
+
+routes.post("/posts", (req, res) => {
+  const post = req.body;
+  res.status(201);
+  res.send("Post created!");
+  posts.push({ ...post, id: uuidv4(), date: new Date() });
+});
+
+routes.put("/posts/:id", (req, res) => {
+  const postId = req.body.id;
+  const { username, title, description, id, date } = req.body;
+
+  const foundPost = posts.find((post) => post.id === postId);
+
+  if (!foundPost) {
+    res.status(404);
+    res.send("There is no post with that ID to update.");
+  } else {
+    posts = posts.map(function (post) {
+      if (post.id === postId) {
+        return {
+          username,
+          title,
+          description,
+          id: post.id,
+          date: new Date(),
+        };
+      }
+      return post;
+    });
+    res.status(200);
+    res.send("Post updated!");
+  }
+});
+
+// routes.get("/posts/:id", (req, res) => {
+//   const { id } = req.params;
+//   const findUser = users.find((user) => user.id === id);
+
+//   if (findUser === undefined) {
+//     return res.status(404).send("Not found!");
+//   }
+//   if (findUser) {
+//     return res.send(findUser);
+//   }
+// });
+
+routes.delete("/posts/:id", (req, res) => {
+  //   const { id } = req.params;
+  //   users = users.filter((users) => users.id !== id);
+
+  //   res.send(`User with ${id} has been deleted`);
+  const postId = req.params.id;
+
+  const foundPost = posts.find((post) => post.id === postId);
+
+  if (!foundPost) {
+    res.status(404);
+    res.send("There is no user with that ID to delete.");
+  } else {
+    posts = posts.filter(function (post) {
+      return post.id !== postId;
+    });
+    res.status(200);
+    res.send("User deleted!");
+  }
+});
+
+// module.exports = routes;
+export default routes;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import userRouter from "./routes/users.js";
-// import bcrypt from "bcrypt";
+import postRouter from "./routes/posts.js";
 
 const app = express();
 const port = 3000;
@@ -8,6 +8,7 @@ const host = "localhost";
 
 app.use(express.json());
 app.use("/", userRouter);
+app.use("/", postRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://${host}:${port}`);
