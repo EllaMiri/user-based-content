627821119696d37b692bf6225f18c4603ce99d09
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-04-21 15:49:55 +0200

    added layout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,21 @@
-import Header from "./Header";
-import Footer from "./Footer";
+import React from "react";
+// import { Route, Routes } from "react-router-dom";
+import Header from "./Layout/header";
+import Footer from "./Layout/footer";
+import Main from "./Layout/main";
+// import Login from "./login";
+// import Register from "./register";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
       <Footer />
+      {/* <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+      </Routes> */}
     </div>
   );
 }
