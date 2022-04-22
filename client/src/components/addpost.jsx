beb6df453d67d186859769e04bfa038571f60d4b
Jannie87 <jannie.backman@gmail.com>
2022-04-22 11:32:25 +0200

    added bootstrap form

--- client/src/components/addpost.jsx
+++ client/src/components/addpost.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 export default function addPost() {
   return (
     <div>
-      <form>
-        <label>Title</label>
-        <input type="text" placeholder="Title"></input>
-        <label>
-          Söker du nytt hem eller vill du adoptera? Information om ditt djur/vad
-          du söker.
-        </label>
-        <textarea rows="4" cols="50"></textarea>
-        <button>Lägg till</button>
-      </form>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group controlId="validation1">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            required
+            isInvalid
+            name="title"
+            type="text"
+            placeholder="title"
+          />
+          <Form.Control.Feedback type="invalid">
+            Skriv in ditt användarnamn
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Form>
+
+      <label>Title</label>
+      <input type="text" placeholder="Title"></input>
+      <label>
+        Söker du nytt hem eller vill du adoptera? Information om ditt djur/vad
+        du söker.
+      </label>
+      <textarea rows="4" cols="50"></textarea>
+      <Button variant="success">Lägg till</Button>
     </div>
   );
 }
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,25 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../components/register.css";
+import { Form, Button } from "react-bootstrap";
 
 export default function Register() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const loginForm = event.currentTarget;
+    if (loginForm.checkValidaity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+
   return (
     <div className="style">
       <h2>Registrera ett konto</h2>
 
-      <form>
-        <label>Username</label>
-        <input name="username" type="text" placeholder="användarnamn" />
-        <label>Email</label>
-        <input name="email" type="text" placeholder="example@mail.com" />
-        <label>Password</label>
-        <input name="password" type="text" placeholder="lösenord" />
-        <button>Registrera</button>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group controlId="validation1">
+          <Form.Label>Användarnamn</Form.Label>
+          <Form.Control
+            required
+            isInvalid
+            name="username"
+            type="text"
+            placeholder="användarnamn"
+          />
+          <Form.Control.Feedback type="invalid">
+            Välj ett användarnamn
+          </Form.Control.Feedback>
+        </Form.Group>
 
-        <Link to="/login">
-          <p>Logga in?</p>
-        </Link>
-      </form>
+        <Form.Group controlId="validation2">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            required
+            isInvalid
+            name="email"
+            type="text"
+            placeholder="email"
+          />
+          <Form.Control.Feedback type="invalid">
+            Skriv in din email
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="validation2">
+          <Form.Label>Lösenord</Form.Label>
+          <Form.Control
+            required
+            isInvalid
+            name="password"
+            type="text"
+            placeholder="lösenord"
+          />
+          <Form.Control.Feedback type="invalid">
+            Skriv in ett lösenord
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Form>
+      <Button variant="success">Registrera</Button>
+
+      <Link to="/login">
+        <p>Logga in?</p>
+      </Link>
     </div>
   );
 }
