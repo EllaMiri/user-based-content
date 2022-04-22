de692d73cb09644b55d26d9e0556b59d0c8edab5
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-04-22 08:31:16 +0200

    fixed the layout

---
+++ client/src/components/Layout/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+        <span className="namesFooter">
+          ©Anna Özmehak, Ella Larsson, Jannie Bäckman Kuurne
+        </span>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ client/src/components/Layout/header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.css"
+
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Cats and dogs</h1>
+
+            <span> Logga in/Registrera dig </span>
+        </div>
+
+    );
+}
+
+
+
+
+export default Header;
