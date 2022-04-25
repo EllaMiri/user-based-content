848f0a4c20fece34b85c42a2cfc7215499330d97
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-04-25 14:22:32 +0200

    fixed design for posts

--- client/src/components/Layout/main.jsx
+++ client/src/components/Layout/main.jsx
@@ -2,21 +2,14 @@ import React from "react";
 import "./main.css";
 import AddPost from "../addpost";
 
-
 function Main() {
-
   return (
     <div className="main">
       <div className="background-box">
-      
-      <div className ="allPosts">
-        <h3 className="mainTitle">Alla djurannonser</h3>
-        <AddPost />
-      </div>
-      <div className="postContainer">
-        <p className="posts">Findus söker nytt hem!</p>
-        <p className="posts">Fluffig hårboll letar nytt hem!</p>
-      </div>
+        <div className="allPosts">
+          <h3 className="mainTitle">Alla djurannonser</h3>
+          <AddPost />
+        </div>
       </div>
     </div>
   );
--- client/src/components/addpost.jsx
+++ client/src/components/addpost.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 export default function AddPost() {
   // let [validated, setValidated] = useFormValidation()
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const [postTitle, setPostTitle] = useState("");
   const [postText, setPostText] = useState("");
@@ -69,7 +69,8 @@ export default function AddPost() {
 
   return (
     <>
-    <Button onClick={() => setOpen(!open)}
+      <Button
+        onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open}
         variant="success"
@@ -78,74 +79,78 @@ export default function AddPost() {
         Ny annons
       </Button>
       <Collapse in={open}>
-       
-    <div id="example-collapse-text" className="addPostContainer">
-      <Form className="formAddPost" noValidate validated={validated} onSubmit={handleSubmit} >
-        <Row className="md-3">
-          <Form.Group as={Col} md="12" controlId="validation1">
-            <Form.Label className="fs-5">Title</Form.Label>
-            <Form.Control
-              size="lg"
-              required
-              type="text"
-              placeholder="Titel"
-              onChange={(e) => {
-                setPostTitle(e.target.value);
-              }}
-              value={postTitle}
-            />
-            <Form.Control.Feedback type="invalid">
-              Skriv in ditt användarnamn
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Row>
+        <div id="example-collapse-text" className="addPostContainer">
+          <Form
+            className="formAddPost"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+          >
+            <Row className="md-3">
+              <Form.Group as={Col} md="12" controlId="validation1">
+                <Form.Label className="fs-5">Title</Form.Label>
+                <Form.Control
+                  size="lg"
+                  required
+                  type="text"
+                  placeholder="Titel"
+                  onChange={(e) => {
+                    setPostTitle(e.target.value);
+                  }}
+                  value={postTitle}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Skriv in ditt användarnamn
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
 
-        <Row className="md-3">
-          <Form.Group as={Col} md="12" controlId="validation2">
-            <Form.Label
-            className="fs-5"
-             >
-              Söker du nytt hem eller vill du adoptera? Information om ditt
-              djur/vad du söker.
-            </Form.Label>
-            <Form.Control
-              size="lg"
-              required
-              as="textarea"
-              placeholder="Information..."
-              type="text"
-              onChange={(e) => {
-                setPostText(e.target.value);
-              }}
-              value={postText}
-            />
-            <Form.Control.Feedback type="invalid">
-              Saknas information
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Row>
-        <Button className="addBtn" variant="success" type="submit">
-          Lägg till
-        </Button>
-      </Form>
+            <Row className="md-3">
+              <Form.Group as={Col} md="12" controlId="validation2">
+                <Form.Label className="fs-5">
+                  Söker du nytt hem eller vill du adoptera? Information om ditt
+                  djur/vad du söker.
+                </Form.Label>
+                <Form.Control
+                  size="lg"
+                  required
+                  as="textarea"
+                  placeholder="Information..."
+                  type="text"
+                  onChange={(e) => {
+                    setPostText(e.target.value);
+                  }}
+                  value={postText}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Saknas information
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Button className="addBtn" variant="success" type="submit">
+              Lägg till
+            </Button>
+          </Form>
+        </div>
+      </Collapse>
       <div>
         {postItems.map((post) => (
-          <div className="posts">
-            <p
-              onClick={() => {
-                deletePost(post._id);
-              }}
-              className="remove-btn"
-            >
-              ❌
-            </p>
-            <p>{post.title}</p>
-            <p>{post.text}</p>
+          <div className="postContainer">
+            <div className="posts">
+              <p
+                onClick={() => {
+                  deletePost(post._id);
+                }}
+                className="remove-btn"
+              >
+                ❌
+              </p>
+              <h4>{post.title}</h4>
+              <p>{post.text}</p>
+            </div>
           </div>
         ))}
       </div>
-    </div>
-          </Collapse>
     </>
   );
 }
